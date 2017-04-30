a0b3033c1717d227f8290712f3ecd3e6b63db20e
Brandon Schaefer <brandon.schaefer@canonical.com>
2017-04-29 17:33:24 -0700

    * Simple working bst

--- src/bst.h
+++ src/bst.h
@@ -23,18 +23,13 @@
  * SOFTWARE.
  */
 
-#ifndef BST_H
-#define BST_H
+#ifndef DS_BST_H_
+#define DS_BST_H_
 
-#include <algorithm>
 #include <memory>
 
 namespace ds
 {
-
-namespace
-{
-}
 
 template <typename T>
 struct Node
@@ -45,128 +40,310 @@ struct Node
     Node* parent{nullptr};
 };
 
-/*
-
-Find:
-    2
-   / \
-  1  3
-
-Node* find(T const& elem)
-
-Insert:
-    
-
-Delete:
-
-*/
-
 template <typename T>
 class bst
 {
 public:
     bst() = default;
 
-    ~bst()
-    {
-        node_allocator.deallocate(root, 1);
-    }
+    ~bst();
 
-    void insert(T const& elem)
-    {
-        if (root == nullptr)
-        {
-            root = node_allocator.allocate(1);
-            node_allocator.construct(root);
+    bool empty() const;
+    size_t size() const;
 
-            root->elem = elem;
-        }
-        else
-        {
-            Node<T>* parent = find(elem);
+    void insert(T const& elem);
+    void remove(T const& elem);
 
-            // We already exists in the tree, ignore dup
-            if (parent->elem != elem)
-            {
-                if (elem < parent->elem)
-                {
-                    parent->left = node_allocator.allocate(1);
-                    node_allocator.construct(parent->left);
+    Node<T>* find(T const& elem) const;
 
-                    parent->left->elem   = elem;
-                    parent->left->parent = parent;
-                }
-                else
-                {
-                    parent->right = node_allocator.allocate(1);
-                    node_allocator.construct(parent->right);
-
-                    parent->right->elem    = elem;
-                    parent->right->parent = parent;
-                }
-            }
-        }
-    }
-
-    bool empty() const
-    {
-        return size() == 0;
-    }
-
-    size_t size() const
-    {
-        return recursive_size(root);
-    }
-
-    Node<T>* find(T const& elem)
-    {
-        return recursive_find(root, elem);
-    }
+    // Print functions
+    void print_in_order() const;
+    void print_pre_order() const;
+    void print_post_order() const;
 
 private:
-    Node<T>* recursive_find(Node<T>* current, T const& elem)
-    {
-        if (current == nullptr)
-        {
-            throw std::runtime_error("Empty tree");
-        }
+    void delete_all_node(Node<T>* current);
 
-        if (elem == current->elem)
-        {
-            return current;
-        }
-        else if (elem < current->elem)
-        {
-            if (current->left == nullptr)
-            {
-                return current;
-            }
+    void swap_subtree(Node<T>* from, Node<T>* to);
 
-            return recursive_find(current->left, elem);
-        }
+    void create_node(Node<T>** node, T const& elem);
 
-        if (current->right == nullptr)
-        {
-            return current;
-        }
+    Node<T>* find_lowest_leaf(Node<T>* current) const;
 
-        return recursive_find(current->right, elem);
-    }
+    Node<T>* recursive_find(Node<T>* current, T const& elem) const;
+    size_t recursive_size(Node<T>* current) const;
 
-    size_t recursive_size(Node<T>* current) const
-    {
-        if (current == nullptr)
-        {
-            return 0;
-        }
-
-        return 1 + recursive_size(current->left) + recursive_size(current->right);
-    }
+    void recursive_print_in_order(Node<T>* current) const;
+    void recursive_print_pre_order(Node<T>* current) const;
+    void recursive_print_post_order(Node<T>* current) const;
 
     std::allocator<Node<T>> node_allocator;
     Node<T>* root{nullptr};
 };
 
+template <typename T>
+bst<T>::~bst()
+{
+    delete_all_node(root);
+}
+
+template <typename T>
+void bst<T>::delete_all_node(Node<T>* current)
+{
+    if (current)
+    {
+        delete_all_node(current->left);
+        delete_all_node(current->right);
+        node_allocator.deallocate(current, 1);
+    }
+}
+
+template <typename T>
+bool bst<T>::empty() const
+{
+    return size() == 0;
+}
+
+template <typename T>
+size_t bst<T>::size() const
+{
+    return recursive_size(root);
+}
+
+template <typename T>
+size_t bst<T>::recursive_size(Node<T>* current) const
+{
+    if (current == nullptr)
+    {
+        return 0;
+    }
+
+    return 1 + recursive_size(current->left) + recursive_size(current->right);
+}
+
+template <typename T>
+Node<T>* bst<T>::find(T const& elem) const
+{
+    return recursive_find(root, elem);
+}
+
+template <typename T>
+Node<T>* bst<T>::recursive_find(Node<T>* current, T const& elem) const
+{
+    if (current == nullptr)
+    {
+        throw std::runtime_error("Empty tree");
+    }
+
+    if (elem == current->elem)
+    {
+        return current;
+    }
+    else if (elem < current->elem)
+    {
+        if (current->left == nullptr)
+        {
+            return current;
+        }
+
+        return recursive_find(current->left, elem);
+    }
+
+    if (current->right == nullptr)
+    {
+        return current;
+    }
+
+    return recursive_find(current->right, elem);
+}
+
+template <typename T>
+void bst<T>::create_node(Node<T>** node, T const& elem)
+{
+    *node = node_allocator.allocate(1);
+    node_allocator.construct(*node);
+
+    (*node)->elem   = elem;
+}
+
+template <typename T>
+void bst<T>::insert(T const& elem)
+{
+    if (root == nullptr)
+    {
+        root = node_allocator.allocate(1);
+        node_allocator.construct(root);
+
+        root->elem = elem;
+    }
+    else
+    {
+        auto parent = find(elem);
+
+        // We already exists in the tree, ignore dup
+        if (parent->elem != elem)
+        {
+            if (elem < parent->elem)
+            {
+                create_node(&parent->left, elem);
+                parent->left->parent = parent;
+            }
+            else
+            {
+                create_node(&parent->right, elem);
+                parent->right->parent = parent;
+            }
+        }
+    }
+}
+
+template <typename T>
+void bst<T>::swap_subtree(Node<T>* from, Node<T>* to)
+{
+    auto parent = from->parent;
+
+    if (parent == nullptr)
+    {
+        root = to;
+    }
+    else
+    {
+        if (parent->left && parent->left == from)
+        {
+            parent->left = to;
+        }
+        else
+        {
+            parent->right = to;
+        }
+    }
+
+    if (to != nullptr)
+    {
+        to->parent = from->parent;
+
+        // Dont wany cycles now do we
+        if (from->left != to)
+        {
+            to->left = from->left;
+        }
+
+        if (from->right != to)
+        {
+            to->right = from->right;
+        }
+    }
+}
+
+template <typename T>
+void bst<T>::remove(T const& elem)
+{
+    auto node_to_delete = find(elem);
+
+    // Otherwise not found
+    if (node_to_delete->elem == elem)
+    {
+        // Leaf case
+        if (node_to_delete->left  == nullptr &&
+            node_to_delete->right == nullptr)
+        {
+            swap_subtree(node_to_delete, nullptr);
+        }
+        // Case 1 child
+        else if (node_to_delete->left  == nullptr ||
+                 node_to_delete->right == nullptr)
+        {
+            if (node_to_delete->left == nullptr)
+            {
+                swap_subtree(node_to_delete, node_to_delete->right);
+            }
+            else
+            {
+                swap_subtree(node_to_delete, node_to_delete->left);
+            }
+        }
+        // Case 2 children
+        else
+        {
+            auto lowest_right = find_lowest_leaf(node_to_delete->right);
+            swap_subtree(node_to_delete, lowest_right);
+        }
+
+        node_allocator.deallocate(node_to_delete, 1);
+    }
+}
+
+template <typename T>
+Node<T>* bst<T>::find_lowest_leaf(Node<T>* current) const
+{
+    if (current->left != nullptr)
+    {
+        return find_lowest_leaf(current->left);
+    }
+    else if (current->right != nullptr)
+    {
+        return find_lowest_leaf(current->right);
+    }
+    else
+    {
+        return current;
+    }
+}
+
+template <typename T>
+void bst<T>::print_in_order() const
+{
+    recursive_print_in_order(root);
+    std::cout << '\n';
+}
+
+template <typename T>
+void bst<T>::print_pre_order() const
+{
+    recursive_print_pre_order(root);
+    std::cout << '\n';
+}
+
+template <typename T>
+void bst<T>::print_post_order() const
+{
+    recursive_print_post_order(root);
+    std::cout << '\n';
+}
+
+template <typename T>
+void bst<T>::recursive_print_in_order(Node<T>* current) const
+{
+    if (current)
+    {
+        recursive_print_in_order(current->left);
+        std::cout << current->elem << ' ';
+        recursive_print_in_order(current->right);
+    }
+}
+
+template <typename T>
+void bst<T>::recursive_print_pre_order(Node<T>* current) const
+{
+    if (current)
+    {
+        std::cout << current->elem << ' ';
+        recursive_print_pre_order(current->left);
+        recursive_print_pre_order(current->right);
+    }
+}
+
+template <typename T>
+void bst<T>::recursive_print_post_order(Node<T>* current) const
+{
+    if (current)
+    {
+        recursive_print_post_order(current->left);
+        recursive_print_post_order(current->right);
+        std::cout << current->elem << ' ';
+    }
+}
+
 } // namespace ds
 
-#endif // QUEUE_H
+#endif // DS_BST_H_
--- tests/test_bst.cpp
+++ tests/test_bst.cpp
@@ -32,27 +32,189 @@ struct TestBST : public ::testing::Test
     ds::bst<int> bst;
 };
 
-TEST_F(TestBST, test_ctor_empty)
+TEST_F(TestBST, ctor_empty)
 {
     EXPECT_TRUE(bst.empty());
 }
 
-TEST_F(TestBST, test_find_empty_tree_throws)
+TEST_F(TestBST, find_empty_tree_throws)
 {
     EXPECT_THROW(bst.find(0), std::runtime_error);
 }
 
-TEST_F(TestBST, test_insert_first)
+TEST_F(TestBST, insert_first)
 {
     ds::bst<int> bst;
+
+    bst.insert(3);
+    EXPECT_EQ(bst.size(), 1u);
+}
+
+TEST_F(TestBST, insert_simple_tree)
+{
+    ds::bst<int> bst;
+
+    bst.insert(2);
+    bst.insert(1);
     bst.insert(3);
 
+    // Find the root and test its correctly constructed
+    auto node = bst.find(2);
+
+    ASSERT_NE(node->left, nullptr);
+    ASSERT_NE(node->right, nullptr);
+
+    EXPECT_EQ(node->left->elem,  1);
+    EXPECT_EQ(node->right->elem, 3);
+    EXPECT_EQ(bst.size(), 3u);
+}
+
+TEST_F(TestBST, remove_leaf_left)
+{
+    ds::bst<int> bst;
+
+    bst.insert(2);
+    bst.insert(1);
+
+    EXPECT_EQ(bst.size(), 2u);
+
+    bst.remove(1);
     EXPECT_EQ(bst.size(), 1u);
+}
+
+TEST_F(TestBST, remove_leaf_right)
+{
+    ds::bst<int> bst;
+
+    bst.insert(1);
+    bst.insert(2);
+
+    EXPECT_EQ(bst.size(), 2u);
+
+    bst.remove(1);
+    EXPECT_EQ(bst.size(), 1u);
+}
+
+TEST_F(TestBST, remove_leaf_root)
+{
+    ds::bst<int> bst;
+
+    bst.insert(3);
+    EXPECT_EQ(bst.size(), 1u);
+
+    bst.remove(3);
+    EXPECT_EQ(bst.size(), 0u);
+}
+
+TEST_F(TestBST, remove_with_one_child_left)
+{
+    ds::bst<int> bst;
+
+    bst.insert(3);
+    bst.insert(2);
+    bst.insert(1);
+
+    EXPECT_EQ(bst.size(), 3u);
+
+    bst.remove(2);
+    EXPECT_EQ(bst.size(), 2u);
+}
+
+TEST_F(TestBST, remove_with_one_child_right)
+{
+    ds::bst<int> bst;
+
+    bst.insert(1);
+    bst.insert(2);
+    bst.insert(3);
+
+    EXPECT_EQ(bst.size(), 3u);
+
+    bst.remove(2);
+    EXPECT_EQ(bst.size(), 2u);
+}
+
+TEST_F(TestBST, remove_with_one_child_root)
+{
+    ds::bst<int> bst;
+
+    bst.insert(3);
+    bst.insert(2);
+    bst.insert(1);
+
+    EXPECT_EQ(bst.size(), 3u);
+
+    bst.remove(3);
+    EXPECT_EQ(bst.size(), 2u);
+}
+
+TEST_F(TestBST, remove_with_two_children)
+{
+    ds::bst<int> bst;
+
+    /*     8
+          /
+         5
+        / \
+       3   6
+    */
+    bst.insert(8);
+    bst.insert(5);
+    bst.insert(6);
+    bst.insert(3);
+
+    EXPECT_EQ(bst.size(), 4u);
+
+    bst.remove(5);
+    EXPECT_EQ(bst.size(), 3u);
+}
+
+TEST_F(TestBST, remove_with_two_children_root)
+{
+    ds::bst<int> bst;
+
+    bst.insert(2);
+    bst.insert(1);
+    bst.insert(3);
+
+    EXPECT_EQ(bst.size(), 3u);
+
+    bst.remove(2);
+    EXPECT_EQ(bst.size(), 2u);
+}
+
+TEST_F(TestBST, find_returns_insert_parent_if_not_found)
+{
+    ds::bst<int> bst;
+
+    bst.insert(2);
+
+    auto node = bst.find(1);
+
+    EXPECT_EQ(node->elem, 2);
+}
+
+TEST_F(TestBST, find_returns_element)
+{
+    ds::bst<int> bst;
+
+    bst.insert(5);
+    bst.insert(7);
+    bst.insert(4);
+    bst.insert(2);
+
+    auto node = bst.find(4);
+
+    EXPECT_EQ(node->elem, 4);
 }
 
 TEST_F(TestBST, test)
 {
     ds::bst<int> bst;
+    bst.insert(2);
     bst.insert(3);
-    std::cout << bst.size() << '\n';
+    bst.insert(1);
+    bst.print_in_order();
+    bst.print_pre_order();
+    bst.print_post_order();
 }
